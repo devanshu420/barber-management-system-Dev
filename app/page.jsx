7fdab6e10285fdeb1edb9137f5b0b3924cdbb44c
Devanshu Sharma <devanshu11122005@gmail.com>
2026-03-12 15:55:51 +0530

    fix page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -29,34 +29,3 @@ export default function HomePage() {
   );
 }
 
-
-// "use client";
-
-// import { Navbar } from "@/components/navbar"
-// import Home from "@/components/hero-section"
-// import { ServicesSection } from "@/components/services-section"
-// import  BussinessRelation from "@/components/BussinessRelation"
-// import  Quality from "@/components/Quality"
-// import AboutService from "@/components/AboutService";
-// import FeautureInclude from "@/components/FeautureInclude";
-// import Footer from "@/components/Footer";
-
-
-// // import  Footer from "@/components/Footer"
-
-
-// export default function HomePage() {
-//   return (
-//     <div className="min-h-screen bg-background">
-//       <Navbar />
-//       <main>
-//         <Home />
-//         <ServicesSection />
-//         <BussinessRelation />
-//         <Quality />
-//         <AboutService />
-//         <Footer />
-//       </main>
-//     </div>
-//   )
-// }
